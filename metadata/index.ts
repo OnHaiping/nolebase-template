997cd226958216ed8eb25ccf6f9b9e857af7a1b4
王海平 <a15066577233@gmail.com>
2024-09-12 22:15:12 +0800

    update

--- metadata/index.ts
+++ metadata/index.ts
@@ -7,9 +7,9 @@ export const siteDescription = '海上生明月，天涯共此时'
 export const include = ['笔记', '生活']
 
 /** Repo */
-export const githubRepoLink = 'https://github.com/nolebase/nolebase'
+export const githubRepoLink = 'https://github.com/OnHaiping/nolebase-template'
 /** Discord */
-export const discordLink = 'https://discord.gg/XuNFDcDZGj'
+// export const discordLink = 'https://discord.gg/XuNFDcDZGj'
 
 /** 无协议前缀域名 */
 export const plainTargetDomain = 'nolebase.ayaka.io'
