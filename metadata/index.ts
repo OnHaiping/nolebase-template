970a5ee7dfb4a26ac6cc03219eddf7e26f0fa7a3
王海平 <a15066577233@gmail.com>
2024-09-12 23:04:51 +0800

    update ins

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,6 +1,6 @@
 /** 文本 */
 export const siteName = 'Nólëbase'
-export const siteShortName = 'haiping'
+export const siteShortName = 'Tenacity'
 export const siteDescription = '海上生明月，天涯共此时'
 
 /** 文档所在目录 */
@@ -9,7 +9,7 @@ export const include = ['笔记', '生活']
 /** Repo */
 export const githubRepoLink = 'https://github.com/OnHaiping/nolebase-template'
 /** Discord */
-export const discordLink = 'https://discord.gg/XuNFDcDZGj'
+export const discordLink = 'https://www.instagram.com/haipingnn/'
 
 /** 无协议前缀域名 */
 export const plainTargetDomain = 'nolebase.ayaka.io'
